96efba86411b94878f131adcb4129ede334f3495
Rachael <rachaelewins@hotmail.com>
2020-07-13 11:28:05 +0100

    fixing tests for correct number output

--- spec/bankSpec.js
+++ spec/bankSpec.js
@@ -1,9 +1,11 @@
 describe('Bank', function(){
 
   var account;
+  var output;
 
   beforeEach(function(){
     account = new Account;
+    output = ""
   });
 
   describe('print', function() {
@@ -15,18 +17,18 @@ describe('Bank', function(){
   describe('deposit(date, amount)', function() {
     it('deposit', function(){
       account.deposit("13/07/2020", 1005)
-      output = "date || credit || debit || balance\n13/07/2020 || 1005 || || 1005"
+      output = "date || credit || debit || balance\n13/07/2020 || 1005.00 || || 1005.00"
       expect(account.printStatement()).toEqual(output);
     });
     it('deposit, 2', function(){
       account.deposit("14/07/2020", 875)
-      output = "date || credit || debit || balance\n14/07/2020 || 875 || || 875"
+      output = "date || credit || debit || balance\n14/07/2020 || 875.00 || || 875.00"
       expect(account.printStatement()).toEqual(output);
     });
     it('two deposits', function(){
       account.deposit("04/07/2020", 775)
       account.deposit("05/07/2020", 350)
-      output = "date || credit || debit || balance\n05/07/2020 || 350 || || 1125\n04/07/2020 || 775 || || 775"
+      output = "date || credit || debit || balance\n05/07/2020 || 350.00 || || 1125.00\n04/07/2020 || 775.00 || || 775.00"
       expect(account.printStatement()).toEqual(output);
     });
   });
@@ -34,7 +36,7 @@ describe('Bank', function(){
   describe('withdraw(date, amount)', function() {
     it('withdraw', function(){
       account.withdraw("15/07/2020", 105)
-      output = "date || credit || debit || balance\n15/07/2020 || || 105 || -105"
+      output = "date || credit || debit || balance\n15/07/2020 || || 105.00 || -105.00"
       expect(account.printStatement()).toEqual(output);
     });
   });
