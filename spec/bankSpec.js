44acc8fe50cb1479809b7b285b2b2e8aadbebad5
Rachael <rachaelewins@hotmail.com>
2020-07-13 11:04:30 +0100

    deposit amount with a date, hardcoded

--- spec/bankSpec.js
+++ spec/bankSpec.js
@@ -1,10 +1,21 @@
 describe('Bank', function(){
 
-  var account = new Account;
+  var account;
+
+  beforeEach(function(){
+    account = new Account;
+  });
 
   describe('print', function() {
     it('output with no transactions', function(){
       expect(account.printStatement()).toEqual("date || credit || debit || balance");
+    });
+  })
+  describe('deposit(date, amount)', function() {
+    it('deposit', function(){
+      account.deposit("13/07/2020", 1005)
+      output = "date || credit || debit || balance/n13/07/2020 || 1005 || || 1005"
+      expect(account.printStatement()).toEqual(output);
     });
   })
 })
--- src/bank.js
+++ src/bank.js
@@ -1,7 +1,15 @@
 function Account() {
-
+  this.transactions = []
  }
 
  Account.prototype.printStatement = function () {
-   return "date || credit || debit || balance"
+   output = "date || credit || debit || balance"
+   this.transactions.forEach( function(transaction) {
+     output += transaction
+   });
+   return output
+ };
+
+ Account.prototype.deposit = function (date, ammount) {
+   this.transactions.push("13/07/2020 || 1005 || || 1005")
  };
