f22635b20108c8592a2d380cfee256e9a0c98cf7
Rachael <rachaelewins@hotmail.com>
2020-07-17 17:33:18 +0100

    tidying spec files

--- spec/bankSpec.js
+++ spec/bankSpec.js
@@ -20,11 +20,6 @@ describe('Bank', function(){
       output = "date || credit || debit || balance\n13/07/2020 || 1005.00 || || 1005.00"
       expect(account.printStatement()).toEqual(output);
     });
-    it('deposit, 2', function(){
-      account.deposit(875, "14/07/2020")
-      output = "date || credit || debit || balance\n14/07/2020 || 875.00 || || 875.00"
-      expect(account.printStatement()).toEqual(output);
-    });
     it('two deposits', function(){
       account.deposit(775, "04/07/2020")
       account.deposit(350, "05/07/2020")
@@ -33,12 +28,7 @@ describe('Bank', function(){
     });
     it('diposit, unspecified date', function(){
       account.deposit(853.25)
-      var today = new Date();
-      var dd = String(today.getDate()).padStart(2, '0');
-      var mm = String(today.getMonth() + 1).padStart(2, '0');
-      var yyyy = today.getFullYear();
-
-      today = dd + '/' + mm + '/' + yyyy;
+      var today = todayFormatted()
       output = `date || credit || debit || balance\n${today} || 853.25 || || 853.25`
       expect(account.printStatement()).toEqual(output);
     });
@@ -52,12 +42,7 @@ describe('Bank', function(){
     });
     it('withdraw, unspecified date', function(){
       account.withdraw(97.5)
-      var today = new Date();
-      var dd = String(today.getDate()).padStart(2, '0');
-      var mm = String(today.getMonth() + 1).padStart(2, '0');
-      var yyyy = today.getFullYear();
-
-      today = dd + '/' + mm + '/' + yyyy;
+      var today = todayFormatted()
       output = `date || credit || debit || balance\n${today} || || 97.50 || -97.50`
       expect(account.printStatement()).toEqual(output);
     });
