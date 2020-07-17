f0b7157a022b474debdbd5717917abbd83e0ef44
Rachael <rachaelewins@hotmail.com>
2020-07-17 14:58:42 +0100

    unit tests for transaction history

--- spec/transactionSpec.js
+++ spec/transactionSpec.js
@@ -7,9 +7,21 @@ describe('TransactionHistory', function(){
     output = ""
   });
 
-  describe('listTransactions', function() {
+  describe('listAll', function() {
     it('output with no transactions', function(){
       output = "date || credit || debit || balance"
+      expect(transactionHistory.listAll()).toEqual(output);
+    });
+  });
+  describe('addTransactions', function() {
+    it('output with deposit', function(){
+      transactionHistory.addTransaction("13/07/2020", 1005, 0, 1005)
+      output = "date || credit || debit || balance\n13/07/2020 || 1005.00 || || 1005.00"
+      expect(transactionHistory.listAll()).toEqual(output);
+    });
+    it('output with withdrawal', function(){
+      transactionHistory.addTransaction("13/07/2020", 0, 56, -56)
+      output = "date || credit || debit || balance\n13/07/2020 || || -56.00 || -56.00"
       expect(transactionHistory.listAll()).toEqual(output);
     });
   });
